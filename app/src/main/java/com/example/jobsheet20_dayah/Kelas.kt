85b1f2b407fcae95e6f344ae179ba8a5412cce77
NorHidayah <salma@gmail.com>
2022-11-14 12:23:48 +0700

    first commit

---
+++ app/src/main/java/com/example/jobsheet20_dayah/Kelas.kt
@@ -0,0 +1,9 @@
+package com.example.jobsheet20_dayah
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+data class Kelas(
+    val nama: String?,
+    val nis: String?
+)
